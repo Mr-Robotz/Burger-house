820aa797c0eab68844afad7b383f58a979b06357
Mr Robotz <mrr.robotz@gmail.com>
2024-09-21 14:31:03 -0700

    Made come changes to the tailwind.config.ts file and some clearn up in the create next-app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,26 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "480px",
+      md:"768px",
+      lg: "1024px",
+    },
     extend: {
+      fontFamily: {
+        alfaSlabOne: ["Alfa Slab One", "san-serif"],
+        bebasNeue: ["Bebas Neue", "san-serif"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "1.5rem",
+        },
       },
     },
   },
